e5b913d6c1a5e92c2b59df75c88d02eb1ae30150
thedevanshshukla <141309134+thedevanshshukla@users.noreply.github.com>
2025-07-09 13:30:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask
+from flask_login import LoginManager
+from datetime import datetime
+from config import Config
+from models import db, User
+from auth.routes import auth_bp
+from dashboard.routes import dashboard_bp
+from flask import render_template as rt
+from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import generate_csrf
+
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+csrf = CSRFProtect(app)
+# Initialize extensions
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@app.route('/')
+def home():
+    return rt('home.html')
+# Flask-Login user loader
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Register blueprints
+app.register_blueprint(auth_bp)
+app.register_blueprint(dashboard_bp)
+
+# Inject current year globally for templates
+@app.context_processor
+def inject_now():
+    return {'current_year': datetime.now().year}
+@app.context_processor
+def inject_csrf_token():
+    return dict(csrf_token=generate_csrf)
+@app.after_request
+def add_cache_control_headers(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
+
+# Create database tables and run
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+
+# change layout
+# filter not working 
+#add delete option
---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+from datetime import datetime, timezone
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    expenses = db.relationship('Expense', backref='user', lazy=True)
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
