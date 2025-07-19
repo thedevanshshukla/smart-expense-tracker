f2546a43220dffe86d69641ab819dc7b4165bccf
thedevanshshukla <141309134+thedevanshshukla@users.noreply.github.com>
2025-07-19 18:20:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def add_cache_control_headers(response):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=False)
 
 
 # change layout
